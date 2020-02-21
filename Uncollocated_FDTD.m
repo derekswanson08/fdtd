7468a9c08c952ffc79dec26e3a89f2f7d1a60976
Derek Swanson <derekswanson08@gmail.com>
2020-02-20 22:28:48 -0800

    code from July 8, 2011

--- Uncollocated_FDTD.m
+++ Uncollocated_FDTD.m
@@ -6,7 +6,9 @@
 % This program uses the Uncollocated FDTD technique to solve for the
 % voltage and current along a 1-D transmission line
 
-clear;
+clear; close all;
+scrnsz = get(0,'ScreenSize');
+figure('Position',[scrnsz(1) scrnsz(2) scrnsz(3) scrnsz(4)])
 
 ustep=@(t) 0.5*(sign(t)+1);
 pulse=@(t) ustep(t+.5) - ustep(t-.5);
@@ -14,8 +16,8 @@ pulse=@(t) ustep(t+.5) - ustep(t-.5);
 % Transmission Line Parameters
 R = 0;
 G = 0;
-C = 250e-9;
-L = 1e-9;
+L = 250e-9;
+C = 1e-9;
 
 % Simulation Parameters
 M = 100;  % Number of Nodes
@@ -24,8 +26,8 @@ Len = 1;
 A = 1;
 
 % Boundary Conditions
-RS = 0;
-RL = 1e9;
+RS = 10;
+RL = 100;
 
 % Wave Propegation Speed
 up = sqrt(1/L/C);
@@ -40,13 +42,13 @@ dt = dz/up;
 z = -Len:dz:0;
 t = 0:dt:(N-1)*dt;
 
-% Sinusoidal Source
-f = 400e6;
-periods = 2;
-vg = A*sin(f*2*pi*t).*(1-ustep(t - 1/f*periods));
+% % Sinusoidal Source
+% f = 400e6;
+% periods = 2;
+% vg = A*sin(f*2*pi*t).*(1-ustep(t - 1/f*periods));
 
-% % Pulse Source
-% vg = 1-ustep(t - 2.5e-9);
+% Pulse Source
+vg = 1-ustep(t - 2.5e-9);
 
 % % DC Source
 % vg = ones(length(t));
@@ -74,6 +76,7 @@ i = zeros(1,M-1);
 vn = zeros(1,M);
 in = zeros(1,M-1);
 
+
 for n = 1:N
 
     % generate plots
@@ -84,7 +87,7 @@ for n = 1:N
     ylabel('voltage (V)');
     title(sprintf('t = %.3f ns',(n-1)*dt*1e9));
     subplot(2,1,2);
-    plot(z(1:M-1)+dt/2,i)%,z,ii);
+    plot(z(1:M-1)+dz/2,i)%,z,ii);
     axis([-Len,0,-.3,.3]);
     xlabel('distance (m)');
     ylabel('current (A)');
@@ -92,27 +95,33 @@ for n = 1:N
 
     % Update Currents
     for m = 1:M-1
-        in(m) = dt/L*((v(m) - v(m+1))/dz - i(m)*R) + i(m);
+        %in(m) = dt/L*((v(m) - v(m+1))/dz - i(m)*R) + i(m);
+        in(m) = ((v(m) - v(m+1))/dz + i(m)*(-R/2 + L/dt))/(R/2 + L/dt);
     end
     
-    % Boundary Condition at the Source
+
+    %vn(1) = vg(n);
     
-    %vn(1) = (v(1)*(-G*RS + C/dt*RS - 1/2/dz) + in(1)*RS/dz + vg(n)/dz)/(1/2/dz + C*RS/dt);
-    vn(1) =(v(1)*(G*RS - C*RS/dt + 1/2/dz) - vg(n)/dz + in(1)*RS/dz)/(-1/2/dz - C/dt*RS);q 
     
     % Update Voltages
     for m = 1:M-2
-        vn(m+1) = dt/C*((in(m) - in(m+1))/dz - v(m+1)*G) + v(m+1);
+        %vn(m+1) = dt/C*((in(m) - in(m+1))/dz - v(m+1)*G) + v(m+1);
+        vn(m+1) = ((in(m) - in(m+1))/dz + v(m+1)*(-G/2 + C/dt))/(G/2 + C/dt);
     end
     
+    % Boundary Condition at the Source
+    if(RS > 0)
+        vn(1) = (v(1)*(G/2 - C/dt + 1/RS/dz) - 2*vg(n)/RS/dz + 2*in(1)/dz)/(-1/RS/dz - G/2 - C/dt);
+    else
+        vn(1) = vg(n);
+    end
+        
     % Boundary Condition at the termination
-    vn(M) = v(M)*(-G*RL + C*RL/dt - 1/2/dz)/(1/2/dz + C*RL/dt);
+    vn(M) = (v(M)*(RL*G/2 - RL*C/dt + 1/dz) - 2*RL*in(M-1)/dz)/(-1/dz - RL*G/2 - RL*C/dt);
     
     % Update arrays
     v = vn;
     i = in;
-    
-    pause
     
 end
 
